d1ca07f158e8f624503be38a678a13c298c02dc4
Akshay Bansal <43522054+200395887@users.noreply.github.com>
2019-05-29 22:48:03 -0400

    Commit1

---
+++ 200395887-Lesson1/200395887-Lesson1/Models/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _200395887_Lesson1.Models
+{
+    public class Class1
+    {
+        public int id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public string Year { get; set; }
+
+    }
+}
---
+++ 200395887-Lesson1/200395887-Lesson1/Views/CarsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _200395887_Lesson1.Views
+{
+    public class CarsController : Controller
+    {
+        // GET: Cars
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
